91ba5375a76fa6eef5fed63668082fd630541c24
Joel <itsjoelpm@gmail.com>
2023-10-31 23:59:52 +0300

    more dummy data

---
+++ src/assets/data/news.ts
@@ -0,0 +1,92 @@
+export const testNews = [
+  {
+    id: 1,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 2,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 3,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 4,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 5,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 6,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 7,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 8,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 9,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 10,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 11,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 12,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 13,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 14,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+  {
+    id: 15,
+    headline:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam doloribus cum illum mollitia voluptas vero?',
+    date: 'Oct 30, 2023',
+  },
+];
