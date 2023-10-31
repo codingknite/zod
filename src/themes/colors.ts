edb36e3e751435503410fe9f2b63b44f9b82056a
Joel <itsjoelpm@gmail.com>
2023-10-31 16:51:22 +0300

    chore: colors

---
+++ src/themes/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  blue: 'abc4ff',
+  primary: '#FB6739',
+  gray: {
+    light: '#495057',
+    dark: '#1e1e24',
+  },
+  white: {
+    light: '#fff',
+    medium: '#f8f9fa',
+  },
+};
