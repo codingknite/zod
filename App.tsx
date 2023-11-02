6f9266c3674a7c98952d1dd4a6b6522f4de1cbbb
Joel <itsjoelpm@gmail.com>
2023-11-02 11:03:13 +0300

    chore: import router

--- App.tsx
+++ App.tsx
@@ -1,28 +1,29 @@
 import React = require('react');
-import Home from './src/screens/Home';
-import Login from './src/screens/Login';
-import Search from './src/screens/Search';
-import SearchResults from './src/screens/SearchResults';
-import {View, StyleSheet} from 'react-native';
-import Signup from './src/screens/Signup';
+import Navigation from './src/navigation';
 
 function App(): JSX.Element {
-  return (
-    <View style={styles.container}>
-      <SearchResults />
-      {/* <Home /> */}
-    </View>
-  );
+  return <Navigation />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-  },
-});
 export default App;
 
-// done -> Home ✅
-// done -> Login ✅
-// done -> Signup ✅
-// done -> Search ✅
+/********** TODO:
+ *
+ * Check whether the user is alreadt logged in on page setup
+ * if logged in the initialRouteName should be Home
+ * if not the initialRouteName should be Onboard
+ *
+ * Another TODO:
+ * Move every screen container to safeareaview
+ * Then style the status bar
+ *
+ * Another TODO:
+ * For the wallet address on the profile page, truncate to include ... in the middle
+ * Add a copy icon
+ *
+ *
+ * Another TODO:
+ * Consider adding a see more button in the transactions card in profile
+ * This way a user can see more stuff like transaction id, etc
+ *
+ * **********/
