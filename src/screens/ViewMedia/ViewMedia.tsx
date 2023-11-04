514470155c15f88027e8dcfb464c32809d2fec38
Joel <itsjoelpm@gmail.com>
2023-11-04 20:10:47 +0300

    view media screen

---
+++ src/screens/ViewMedia/ViewMedia.tsx
@@ -0,0 +1,211 @@
+/* eslint-disable react-native/no-inline-styles */
+import styles from './styles';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+  Pressable,
+} from 'react-native';
+import {colors} from '../../themes/colors';
+import axios, {AxiosResponse} from 'axios';
+import searchBarStyles from '../Home/styles';
+import TopNav from '../../components/TopNav';
+import {useRoute} from '@react-navigation/native';
+import BottomNav from '../../components/BottomNav';
+import {useNavigation} from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+interface TransactionTag {
+  name: string;
+  value: string;
+}
+
+interface TransactionData {
+  size: string;
+  type: string;
+}
+
+interface TransactionNode {
+  id: string;
+  tags: TransactionTag[];
+  data: TransactionData;
+}
+
+interface TransactionEdge {
+  node: TransactionNode;
+}
+
+interface TransactionResponseData {
+  transactions: {
+    edges: TransactionEdge[];
+  };
+}
+
+interface ApiResponseData {
+  status: number;
+  data: TransactionResponseData;
+  errors: null;
+}
+
+interface ApiResponse {
+  data: ApiResponseData;
+  message: string;
+}
+
+const ViewMedia = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
+  const [transactionData, setTransactionData] = useState<TransactionEdge[]>([]);
+  const {transactionId} = route.params;
+
+  const [imageDimensions, setImageDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    Image.getSize(
+      `https://arweave.net/${transactionId}`,
+      (width, height) => {
+        setImageDimensions({width, height});
+      },
+      error => {
+        console.error('Error getting image size: ', error);
+      },
+    );
+  }, [transactionId]);
+
+  useEffect(() => {
+    const fetchTxn = async () => {
+      try {
+        const postData = {
+          transactionId,
+          gateway: '',
+        };
+
+        const queryResponse: AxiosResponse<ApiResponse> = await axios.post(
+          'http://localhost:3000/api/query-transaction',
+          postData,
+        );
+
+        const responseData = queryResponse.data;
+        setTransactionData(responseData.data.data.transactions.edges);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+
+    fetchTxn();
+  }, [transactionId]);
+
+  const tags = transactionData.length > 0 && transactionData[0].node.tags;
+
+  const openViewBlock = (txnId: string) => {
+    navigation.navigate('Webview', {
+      link: `https://viewblock.io/arweave/tx/${txnId}`,
+    });
+  };
+
+  const openBazar = (txnId: string) => {
+    navigation.navigate('Webview', {
+      link: `https://bazar.ar-io.dev/#/asset/${txnId}`,
+    });
+  };
+
+  const imageWidth = imageDimensions.width > 0 ? imageDimensions.width : 1;
+  const imageHeight = imageDimensions.width > 0 ? imageDimensions.height : 1;
+
+  console.log(transactionId);
+
+  return (
+    <View style={searchBarStyles.container}>
+      <TopNav />
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="small" color={colors.white.light} />
+        </View>
+      ) : (
+        <ScrollView style={styles.contentArea}>
+          <Pressable
+            style={styles.backButton}
+            onPress={() => {
+              navigation.goBack();
+            }}>
+            <Ionicons name="chevron-back-outline" color="#246bee" size={22} />
+            <Text style={styles.backText}>Back</Text>
+          </Pressable>
+          <Image
+            source={{uri: `https://arweave.net/${transactionId}`}}
+            style={[
+              styles.image,
+              {
+                width: '90%',
+                aspectRatio: imageWidth / imageHeight,
+              },
+            ]}
+          />
+
+          <View style={styles.tagsContainer}>
+            {tags &&
+              tags.map((tag, index) => {
+                if (tag.name === 'Title') {
+                  return (
+                    <Text style={styles.title} key={index}>
+                      {tag.value}
+                    </Text>
+                  );
+                }
+              })}
+
+            <View style={styles.links}>
+              <Pressable onPress={() => openViewBlock(transactionId)}>
+                <Text style={styles.txnId}>ViewBlock ↗</Text>
+                <View style={styles.linkUnderline} />
+              </Pressable>
+
+              <Pressable onPress={() => openBazar(transactionId)}>
+                <Text style={styles.txnId}>Bazar ↗</Text>
+                <View style={styles.linkUnderline} />
+              </Pressable>
+            </View>
+
+            <View style={styles.tagsView}>
+              {tags &&
+                tags.map((tag, index) => {
+                  if (
+                    tag.name === 'Description' ||
+                    tag.name === 'Collection-Code' ||
+                    tag.name === 'Indexed-By' ||
+                    tag.name === 'License' ||
+                    tag.name === 'License-Fee' ||
+                    tag.name === 'Commercial-Use' ||
+                    tag.name === 'Payment-Mode' ||
+                    tag.name === 'Access' ||
+                    tag.name === 'Creator' ||
+                    tag.name === 'Derivation' ||
+                    tag.name === 'App-Name'
+                  ) {
+                    return (
+                      <View style={styles.tag} key={index}>
+                        <Text style={styles.tagName}>{tag.name}</Text>
+                        <Text style={styles.tagValue}>{tag.value}</Text>
+                      </View>
+                    );
+                  }
+                })}
+            </View>
+          </View>
+        </ScrollView>
+      )}
+
+      <BottomNav />
+    </View>
+  );
+};
+
+export default ViewMedia;
---
+++ src/screens/ViewMedia/styles.ts
@@ -0,0 +1,91 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '../../themes/colors';
+
+const styles = StyleSheet.create({
+  backButton: {
+    gap: 8,
+    padding: 12,
+    paddingVertical: 20,
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: colors.black.onboardScreen,
+  },
+  backText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: colors.blue.medium,
+  },
+  contentArea: {
+    flex: 1,
+    paddingTop: 10,
+    backgroundColor: colors.black.onboardScreen,
+  },
+  title: {
+    fontSize: 26,
+    marginBottom: 15,
+    fontWeight: '700',
+    color: colors.textGray,
+  },
+  description: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: colors.textGray,
+    marginBottom: 15,
+  },
+  txnId: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: colors.blue.medium,
+  },
+  image: {
+    width: '50%',
+    alignSelf: 'center',
+  },
+  tagsContainer: {
+    width: '94%',
+    alignSelf: 'center',
+    marginTop: 25,
+  },
+  links: {
+    gap: 12,
+    flexDirection: 'row',
+    paddingVertical: 8,
+  },
+  linkUnderline: {
+    height: 2,
+    width: '100%',
+    backgroundColor: colors.blue.medium,
+  },
+  tagsView: {
+    marginTop: 30,
+    marginBottom: 50,
+  },
+  tag: {
+    gap: 5,
+    padding: 12,
+    marginBottom: 15,
+    borderRadius: 12,
+    backgroundColor: '#16191c',
+  },
+
+  tagName: {
+    fontSize: 14,
+    fontWeight: '700',
+    color: colors.primary,
+  },
+  tagValue: {
+    fontSize: 17,
+    fontWeight: '700',
+    paddingVertical: 4,
+    color: colors.white.light,
+    // color: colors.blue.light,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.black.onboardScreen,
+  },
+});
+
+export default styles;
