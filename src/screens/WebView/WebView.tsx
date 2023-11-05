79238ab2617337a319edd8c278a139321117356b
Joel <itsjoelpm@gmail.com>
2023-11-05 20:08:54 +0300

    add link to nav bar

--- src/screens/WebView/WebView.tsx
+++ src/screens/WebView/WebView.tsx
@@ -23,8 +23,6 @@ const Webview = () => {
     navigation.navigate('Search');
   };
 
-  console.log('LINK', link);
-
   return (
     <View style={styles.container}>
       <View style={styles.searchContainer}>
